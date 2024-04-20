1d0864d98431e24be704140b8c59a5fa52e68cec
Taha <tahamulla@hotmail.com>
2024-04-20 14:15:23 -0400

    Implement game loop and winner determination in main file

--- main.rb
+++ main.rb
@@ -1,3 +1,24 @@
 require_relative 'player'
 require_relative 'game'
 require_relative 'question'
+
+# Create players
+player1 = Player.new("Player 1")
+player2 = Player.new("Player 2")
+
+# Initialize game
+game = Game.new(player1, player2)
+
+# Game loop
+while !game.game_over?
+  current_question = Question.new
+  current_question.ask_question(game.current_player)
+
+  game.switch_player
+end
+
+# Determine winner and display result
+winner = game.winner
+puts "#{winner.name} wins with a score of #{winner.lives}/3"
+puts "------ GAME OVER ------"
+puts "Goodbye!"
