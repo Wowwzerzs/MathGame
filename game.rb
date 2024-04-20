83b9494b7adf1c3af9577dc2f32a64836bd05582
Taha <tahamulla@hotmail.com>
2024-04-20 14:11:20 -0400

    Initialize Game class with methods for managing players and game flow

---
+++ game.rb
@@ -0,0 +1,18 @@
+class Game
+  def initialize(player1, player2)
+    @players = [player1, player2]
+    @current_player = @players.first
+  end
+
+  def switch_player
+    @current_player = @players.rotate!.first
+  end
+
+  def game_over?
+    @players.any? { |player| !player.alive? }
+  end
+
+  def winner
+    @players.find { |player| player.alive? }
+  end
+end
